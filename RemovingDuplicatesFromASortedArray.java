d14781157bebf12514dec14b6fda8d1f8ab16fbf
Dadoright <donabusi1@gmail.com>
2022-11-12 15:06:19 +0530

    Removing Duplicates using a java array

--- RemovingDuplicatesFromASortedArray.java
+++ RemovingDuplicatesFromASortedArray.java
@@ -1,8 +1,37 @@
 import java.util.Scanner;
 import java.util.LinkedList;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream; 
 
 class RemovingDuplicatesFromASortedArray{
+    public static int[] removeElement(int []nums, int index){
+        if (nums == null || index < 0 || index > nums.length) { 
+            System.out.println("non-existing index");
+            return nums; 
+        } 
+        //array to arrayList
+        ArrayList <Integer> a1 = new ArrayList<Integer>();
+        a1 = (ArrayList<Integer>) IntStream.of(nums).boxed().collect(Collectors.toList()); 
+        // Remove the specified element 
+        a1.remove(index); 
+     
+        // return the resultant array 
+        return a1.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public int removeDuplicates(int[] nums) {
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] == nums[i+1]){
+                removeElement(nums,i+1);
+                i = i-1;
+            }
+        }
+
+        return nums.length;
+    }
+
+
     public static void removeDuplicate(LinkedList <Integer> array1){
         for(Integer i=0;i<(array1.size()-1);i++){
             if(array1.get(i)==array1.get(i+1)){
@@ -50,7 +79,7 @@ class RemovingDuplicatesFromASortedArray{
         }
 
         s1.close();
-
-        //System.out.println("Hello there");
     }
 }
+
+    
--- Solution.java
+++ Solution.java
@@ -1,16 +1,31 @@
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream; 
+
 public class Solution {
-    
+        public static int[] removeElement(int []nums, int index){
+            if (nums == null || index < 0 || index > nums.length) { 
+                System.out.println("non-existing index");
+                return nums; 
+            } 
+            //array to arrayList
+            ArrayList <Integer> a1 = new ArrayList<Integer>();
+            a1 = (ArrayList<Integer>) IntStream.of(nums).boxed().collect(Collectors.toList()); 
+            // Remove the specified element 
+            a1.remove(index); 
+         
+            // return the resultant array 
+            return a1.stream().mapToInt(Integer::intValue).toArray();
+        }
+
         public int removeDuplicates(int[] nums) {
-            List<Integer> list = Arrays.asList(nums);
-            LinkedList<String> linkedList = new LinkedList<String>(list);
-            for(Integer i=0;i<(linkedList.size()-1);i++){
-                if(linkedList.get(i)== linkedList.get(i+1)){
-                    linkedList.remove(i+1);
-                    i=i-1;
+            for(int i = 0;i<nums.length;i++){
+                if(nums[i] == nums[i+1]){
+                    removeElement(nums,i+1);
+                    i = i-1;
                 }
-            };
-            nums = linkedList.toArray();
-            return(nums.length);
+            }
+
+            return nums.length;
         }
 }
